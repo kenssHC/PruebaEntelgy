44bfbcde0503603d8f87f2155eb83a6a58ceed1a
kenssHC <kenss54321@gmail.com>
2026-01-04 21:29:32 -0500

    feat: agregar indice de exportacion de servicios

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+/**
+ * Services Index
+ * 
+ * Punto de entrada para todos los servicios de la aplicacion.
+ * Facilita las importaciones desde otros modulos.
+ */
+
+export { CountryService } from './api.service.js';
+export { stateService } from './state.service.js';
